bfa028a79312aad697c26ac9a1805b6bf1e12b37
Hilda Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2025-08-15 15:11:23 +0000

    navbar complete

--- zada-main/src/components/Navbar.jsx
+++ zada-main/src/components/Navbar.jsx
@@ -1,57 +1,111 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "../styles/navbar.css";
 import zadaLogo from "../assets/zada-vector-logo.svg";
 import zadaText from "../assets/zada-full-vector-logo.svg";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Handle Page Reload when Nav Link is active
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    navigate(path);
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <div id="nav">
         <div id="logo">
-          <a id="zada-logo" href="#">
+          <NavLink id="zada-logo" to="/" end>
             <img src={zadaLogo} alt="Zada_Logo" loading="active" />
-          </a>
+          </NavLink>
 
-          <a id="zada-text" href="#">
+          <NavLink id="zada-text" to="/" end>
             <img src={zadaText} alt="Zada Afrique" loading="active" />
-          </a>
+          </NavLink>
         </div>
 
         <div id="navbar">
-          <div id="hamburger-menu">
-            <span class="material-symbols-outlined">
-              dehaze
-            </span>
+          <div
+            id="hamburger-menu"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            <span className="material-symbols-outlined">dehaze</span>
           </div>
 
-          <ul id="nav-links">
-            <div id="close-menu">
-              <span class="material-symbols-outlined">
-                close
-              </span>
+          <ul id="nav-links" className={`${isOpen ? "active" : ""}`}>
+            <div
+              id="close-menu"
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            >
+              <span className="material-symbols-outlined">close</span>
             </div>
-              
+
             <li className="nav-link">
-              <NavLink to="/" end>
+              <NavLink
+                to="/"
+                end
+                onClick={() => {
+                  setIsOpen(false);
+                  handleClick("/");
+                }}
+              >
                 Home
               </NavLink>
             </li>
 
             <li className="nav-link">
-              <NavLink to="/collections">Collections</NavLink>
+              <NavLink
+                to="/collections"
+                onClick={() => {
+                  setIsOpen(false);
+                  handleClick("/collections");
+                }}
+              >
+                Collections
+              </NavLink>
             </li>
 
             <li className="nav-link">
-              <NavLink to="/about">About</NavLink>
+              <NavLink
+                to="/about"
+                onClick={() => {
+                  setIsOpen(false);
+                  handleClick("/about");
+                }}
+              >
+                About
+              </NavLink>
             </li>
 
             <li className="nav-link">
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink
+                to="/contact"
+                onClick={() => {
+                  setIsOpen(false);
+                  handleClick("/contact");
+                }}
+              >
+                Contact
+              </NavLink>
             </li>
 
             <li className="nav-link cart">
-              <NavLink to="/shop">
+              <NavLink
+                to="/shop"
+                onClick={() => {
+                  setIsOpen(false);
+                  handleClick("/shop");
+                }}
+              >
                 <span className="material-symbols-outlined">shopping_cart</span>
               </NavLink>
             </li>
