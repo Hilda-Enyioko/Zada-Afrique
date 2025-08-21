246af1ac6337429ec7daf28083dd97e752b50bc1
Hilda Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2025-08-21 13:29:19 +0000

    save home changes

--- zada-main/src/pages/Home.jsx
+++ zada-main/src/pages/Home.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import zadaLogo from "../assets/logos/zada-vector-logo.svg";
 import zadaText from "../assets/logos/zada-full-vector-logo.svg";
-import heroImage from "../assets/product-images/two-piece2-far.jpg"
+import heroImage from "../assets/product-images/two-piece2-resized.jpg";
 import "../styles/home.css";
 
 function Home() {
   return (
     <>
       <section className="hero-section">
-
         <div className="hero-image">
-          <img src={ heroImage } loading="active" alt="Shop Zada Afrique" />
+          <img src={heroImage} loading="active" alt="Shop Zada Afrique" />
         </div>
 
         <div className="hero-overlay">
@@ -21,7 +20,6 @@ function Home() {
             <span className="material-symbols-outlined">shopping_cart</span>
           </button>
         </div>
-
       </section>
       <section className="top-picks"></section>
       <section className="reviews"></section>
